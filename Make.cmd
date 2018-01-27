af4985b250618d22fa8c13dcbccaedb458ebdfcc
Kirbseverywhere <35226583+Kirbseverywhere@users.noreply.github.com>
2018-01-27 21:27:04 +0100

    Oops

--- Make.cmd
+++
@@ -1,7 +0,0 @@
-cd %~dp0
-copy "FE8U.gba" "FE_Hack.gba"
-cd "%~dp0ups"
-ups apply -b "%~dp0FE_Hack.gba" -p "%~dp0ups\BaseRom.ups" -o "%~dp0FE_Hack.gba"
-cd "%~dp0Event Assembler"
-Core A FE8 "-output:%~dp0FE_Hack.gba" "-input:%~dp0Buildfile.event"
-pause
